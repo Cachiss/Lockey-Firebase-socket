581132958330417498dcd7a1f272090faa51141e
Marcos Camacho <marcoscachis@gmail.com>
2023-04-05 17:18:12 -0600

    hash passwort when is sent to socket

--- services/firebase_service.js
+++ services/firebase_service.js
@@ -1,11 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore, collection, getDocs, addDoc, doc } from "firebase/firestore";
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import fs from 'fs';
-
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';dotenv.config();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const firebaseConfig = JSON.parse(fs.readFileSync(join(__dirname, '..', 'lockey_firebase.json'), 'utf8'));
@@ -15,8 +16,22 @@ const db = getFirestore(app);
 
 export const getUsers = async () => {
   const querySnapshot = await getDocs(collection(db, "users"));
-  //print users in a json
-  return querySnapshot.docs.map(doc => doc.data());
+
+  return querySnapshot.docs.map(doc => {
+    //convert string to number example
+    const {name, email, password} = doc.data();
+    const hash = bcrypt.hashSync(password, parseInt(process.env.BCRYPT_SALT_ROUNDS), (err, hash) => {
+      if(err) throw err;
+      return hash;
+    });
+    return {
+      id: doc.id,
+      name,
+      email,
+      password: hash
+    }
+  });
+
 };
 
 export const deleteUser = async (id) => {
@@ -29,6 +44,6 @@ export const updateUser = async (id, data) => {
 
 export const createUser = async (data) => {
   await addDoc(collection(db, "users"), data);
+  
 }
-
 
